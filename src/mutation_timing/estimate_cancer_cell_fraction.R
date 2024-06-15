011ef3b2e6743a7462c52bffa04f2d85a0fddc2f
Andrew Pregnall <ampregnall@gmail.com>
2024-06-15 10:45:19 -0400

    Update estimate_cancer_cell_fraction.R
    Updated script to run with Snakemake pipeline and edited estimate_cancer_cell_fraction function to handle edge case where CNt = 0.

--- src/mutation_timing/estimate_cancer_cell_fraction.R
+++ src/mutation_timing/estimate_cancer_cell_fraction.R
@@ -1,14 +1,38 @@
 library(tidyverse)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-s", "--snv"),
+              type = "character",
+              default = NULL,
+              help = "Path to data frame containing somatic variants"),
+  make_option(c("-c", "--cnv"),
+              type = "character",
+              default = NULL,
+              help = "Path to data frame containing copy number variants"),
+  make_option(c("-m", "--metadata"),
+              type = "character",
+              default = NULL,
+              help = "Path to data frame containing sample purity estimates"),
+  make_option(c("--sample"), 
+              type = "character",
+              default = NULL,
+              help = "Sample name"),
+  make_option(c("-o", "--output"),
+              type = "character",
+              default = "out.txt",
+              help = "Output file name: [default = %default]"))
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
 ### Load somatic and copy number variants 
-copy_number_vars <- readr::read_delim("data/raw/cnvs/sequenza/PP002-DZ3A_segments.txt", delim = "\t")
-somatic_vars <- readr::read_csv("data/raw/snvs/PP002-DZ3A.ffpe_scenario.local-fdr.paired_somatic.vep.report.csv")
+copy_number_vars <- readr::read_delim(opt$cnv, delim = "\t")
+somatic_vars <- readr::read_csv(opt$snv)
 
 ### Load purity and ploidy estimates for samples
-# estimates <- readxl::read_xlsx(opt$metadata) 
-estimates <- readxl::read_xlsx("metadata/Sequenza-Ploidy-Estimates.xlsx") 
-purity <- estimates[estimates$sample == "PP002-DZ3A",]$cellularity
-ploidy <- estimates[estimates$sample == "PP002-DZ3A",]$ploidy
+estimates <- readxl::read_xlsx(opt$metadata) 
+purity <- estimates[estimates$sample == opt$sample,]$cellularity
 
 ### Create unique mutation ID and select distinct mutations
 mutations <- somatic_vars %>% dplyr::mutate(MutID = stringr::str_c(Chr, Start, Gene, REF, ALT, sep = "-")) %>%
@@ -16,9 +40,10 @@ mutations <- somatic_vars %>% dplyr::mutate(MutID = stringr::str_c(Chr, Start, G
   dplyr::distinct(MutID, .keep_all = TRUE) %>%
   dplyr::mutate(Chr = as.character(Chr))
 
-### Extract copy number info for each mutation
-cnv_per_mutation <- mutations %>% dplyr::left_join(copy_number_vars, by=c("Chr"="chromosome"), relationship = "many-to-many") %>%
-  dplyr::filter(Start >= start.pos & Start <= end.pos) 
+### Extract copy number info for each mutation; remove variants on X chromosome
+cnv_per_mutation <- mutations %>% 
+  dplyr::left_join(copy_number_vars, by=c("Chr"="chromosome"), relationship = "many-to-many") %>%
+  dplyr::filter(Start >= start.pos & Start <= end.pos) %>% dplyr::filter(Chr != "X")
 
 ### Estimate 95% CI of VAF
 cnv_per_mutation <- cnv_per_mutation %>% dplyr::rowwise() %>%
@@ -31,38 +56,44 @@ cnv_per_mutation <- cnv_per_mutation %>% dplyr::rowwise() %>%
   dplyr::mutate(Mutation.Multiplicity.95 = (Tumor.AltFrac.95 / purity) * ((purity * CNt)+ 2 * (1 - purity))) %>%
   dplyr::mutate(Mutation.Multiplicity.05 = (Tumor.AltFrac.05 / purity) * ((purity * CNt) + 2 * (1 - purity)))
 
-### Get maximum copy number from sample
-max_cn <- max(cnv_per_mutation$CNt)
-
 estimate_cancer_cell_fraction <- function(n_tumor_alt, n_tumor, CNt) {
-  
+  # TODO: move function into own file? 
+  # TODO: add more documentation below to explain steps
   # Calculate distribution of VAFs assuming CCF of x: (x*purity) / (2*(1-purity)+purity*CNt
   # Calculate probability of each CCF given tumor/total reads using binomial probability distribution
-  x <- dbinom(n_tumor_alt, n_tumor, prob = sapply(seq(0.01, 1, 0.01), function(x) (x*purity) / (2*(1-purity)+purity*CNt)))
-  
-  if(min(x) == 0) {
-    x[length(x)] <- 1}
-  names(x) <- seq(0.01, 1, 0.01)
-  
-  # Normalize probability distribution
-  x_norm <- x / sum(x) 
-  
-  
-  x_sort <- sort(x_norm, decreasing = TRUE)
-  x_cumalative_likelihood <- cumsum(x_sort)
-  
-  n = sum(x_cumalative_likelihood < 0.95) + 1
-  threshold <- x_sort[n]
-  conf_int <- x[x_norm >= threshold]
-  cancer_cell_fractions <- as.numeric(names(conf_int))
-  ccf <- cancer_cell_fractions[which.max(conf_int)]
-  ccf.05 <- cancer_cell_fractions[1]
-  ccf.95 <- cancer_cell_fractions[length(cancer_cell_fractions)]
-  prob_subclonal <- sum(x_norm[1:90])
-  prob_clonal <- sum(x_norm[91:100])
-  results <- list(ccf, ccf.05, ccf.95, prob_clonal, prob_subclonal)
-  names(results) <- c("CCF", "CCF.05", "CCF.95", "Prob.Clonal", "Prob.Subclonal")
-  results
+  if (CNt == 0) {
+    results <- list(NA, NA, NA, NA, NA)
+    names(results) <- c("CCF", "CCF.05", "CCF.95", "Prob.Clonal", "Prob.Subclonal")
+    return(results)
+  } 
+  else {
+    x <- dbinom(n_tumor_alt, n_tumor, 
+                prob = sapply(seq(0.01, 1, 0.01), function(x) (x*purity) / (2*(1-purity)+purity*CNt)))
+    
+    if(min(x) == 0) {
+      x[length(x)] <- 1}
+    names(x) <- seq(0.01, 1, 0.01)
+    
+    # Normalize probability distribution
+    x_norm <- x / sum(x) 
+    
+    
+    x_sort <- sort(x_norm, decreasing = TRUE)
+    x_cumalative_likelihood <- cumsum(x_sort)
+    
+    n = sum(x_cumalative_likelihood < 0.95) + 1
+    threshold <- x_sort[n]
+    conf_int <- x[x_norm >= threshold]
+    cancer_cell_fractions <- as.numeric(names(conf_int))
+    ccf <- cancer_cell_fractions[which.max(conf_int)]
+    ccf.05 <- cancer_cell_fractions[1]
+    ccf.95 <- cancer_cell_fractions[length(cancer_cell_fractions)]
+    prob_subclonal <- sum(x_norm[1:90])
+    prob_clonal <- sum(x_norm[91:100])
+    results <- list(ccf, ccf.05, ccf.95, prob_clonal, prob_subclonal)
+    names(results) <- c("CCF", "CCF.05", "CCF.95", "Prob.Clonal", "Prob.Subclonal")
+    return(results)
+  }
 }
 
 ### Calculate CCF for the sample
@@ -71,45 +102,51 @@ cnv_per_mutation <- cnv_per_mutation %>% dplyr::rowwise() %>%
   tidyr::unnest_wider(results) %>%
   dplyr::ungroup()
 
+### SAVE RESULTS
+readr::write_delim(cnv_per_mutation, opt$output, delim="\t")
+
 # SANDBOX -----------------------------------------------------------------
 
-### Define function to create combinations of A/B alleles; borrowed from Sequenza 3.0
-mufreq.types.matrix <- function(CNt.min, CNt.max, CNn = 2) {
-  cn_ratio_vect <- seq(from = CNt.min / CNn, to = CNt.max / CNn, by = 1 / CNn)
-  CNt <- cn_ratio_vect * CNn
-  mut_comb <- lapply(CNt, FUN = function(x) seq(from = 0, to = x))
-  times_muts <- sapply(mut_comb, length)
-  data.frame(CNn = CNn, CNt = rep(CNt, times = times_muts),
-             Mt = unlist(mut_comb))
-}
+### Get maximum copy number from sample
+#max_cn <- max(cnv_per_mutation$CNt)
 
-### Define matrix; filter for at least 1 B allele; calculate theoretical frequencies
-types <- mufreq.types.matrix(CNt.min = 1, CNt.max = max_cn, CNn = 2)
-types <- types %>% dplyr::filter(Mt >= 1)
-
-### Define function to calculate theoretical mutation frequency at various copy number states
-theoretical.mufreq <- function(Mt, CNt, CNn = 2, cellularity) {
-  normal.alleles <- (CNt - Mt) * cellularity + CNn * (1 - cellularity)
-  all.alleles    <- (CNt * cellularity) + CNn * (1 - cellularity)
-  1 - (normal.alleles / all.alleles)
-}
-
-### Calculate expected mutation frequency at various copy number states
-types <- types %>% dplyr::rowwise() %>%
-  dplyr::mutate(test = theoretical.mufreq(Mt = Mt, CNt, CNn, cellularity = purity))
-
-
-mufreq.dpois <- function(mufreq, mufreq.model, depth.t, seq.errors = 0.01) {
-  mufreq.model[mufreq.model == 0] <- seq.errors
-  n.success <- round(mufreq * depth.t, 0)
-  dpois(x = n.success, lambda = mufreq.model * depth.t)
-}
-
-get.Mt <- function(F, depth.t, types, CNt, CNn, Mt){
-  types <- types[types$CNn == CNn, ]
-  l <- mufreq.dpois(mufreq = F, types$F[types$CNt== CNt&types$Mt<=Mt], depth.t = depth.t)
-  l <- l/sum(l)
-  L <- data.frame(l = l, Mt = types$Mt[types$CNt== CNt&types$Mt<=Mt])
-  return(L)
-}
-
+# ### Define function to create combinations of A/B alleles; borrowed from Sequenza 3.0
+# mufreq.types.matrix <- function(CNt.min, CNt.max, CNn = 2) {
+#   cn_ratio_vect <- seq(from = CNt.min / CNn, to = CNt.max / CNn, by = 1 / CNn)
+#   CNt <- cn_ratio_vect * CNn
+#   mut_comb <- lapply(CNt, FUN = function(x) seq(from = 0, to = x))
+#   times_muts <- sapply(mut_comb, length)
+#   data.frame(CNn = CNn, CNt = rep(CNt, times = times_muts),
+#              Mt = unlist(mut_comb))
+# }
+# 
+# ### Define matrix; filter for at least 1 B allele; calculate theoretical frequencies
+# types <- mufreq.types.matrix(CNt.min = 1, CNt.max = max_cn, CNn = 2)
+# types <- types %>% dplyr::filter(Mt >= 1)
+# 
+# ### Define function to calculate theoretical mutation frequency at various copy number states
+# theoretical.mufreq <- function(Mt, CNt, CNn = 2, cellularity) {
+#   normal.alleles <- (CNt - Mt) * cellularity + CNn * (1 - cellularity)
+#   all.alleles    <- (CNt * cellularity) + CNn * (1 - cellularity)
+#   1 - (normal.alleles / all.alleles)
+# }
+# 
+# ### Calculate expected mutation frequency at various copy number states
+# types <- types %>% dplyr::rowwise() %>%
+#   dplyr::mutate(test = theoretical.mufreq(Mt = Mt, CNt, CNn, cellularity = purity))
+# 
+# 
+# mufreq.dpois <- function(mufreq, mufreq.model, depth.t, seq.errors = 0.01) {
+#   mufreq.model[mufreq.model == 0] <- seq.errors
+#   n.success <- round(mufreq * depth.t, 0)
+#   dpois(x = n.success, lambda = mufreq.model * depth.t)
+# }
+# 
+# get.Mt <- function(F, depth.t, types, CNt, CNn, Mt){
+#   types <- types[types$CNn == CNn, ]
+#   l <- mufreq.dpois(mufreq = F, types$F[types$CNt== CNt&types$Mt<=Mt], depth.t = depth.t)
+#   l <- l/sum(l)
+#   L <- data.frame(l = l, Mt = types$Mt[types$CNt== CNt&types$Mt<=Mt])
+#   return(L)
+# }
+# 
