c5610061c7004e0086ef28434671b0cc613e2b59
Andrew Pregnall <ampregnall@gmail.com>
2024-09-25 19:34:43 -0400

    Create relatedness.R

---
+++ src/supplementary_analysis/relatedness.R
@@ -0,0 +1,63 @@
+library(tidyverse)
+library(ggpubr)
+library(ggbeeswarm)
+
+
+df <- readxl::read_xlsx("results/ibd_scores/PPGL-IBD.xlsx", sheet = "paired")
+df$cat <- "cat" # Dummy variable for plotting
+tumor_metadata <- readxl::read_xlsx("metadata/mPPGL-Metadata.xlsx", sheet = "tumors")
+
+df2 <- readr::read_delim("data/processed/snvs/drivers/PPGL.somatic.data.combined.filtered.txt")
+df2 <- df2 %>% dplyr::select(c(1:6, 8))
+df2 <- df2 %>% dplyr::left_join(tumor_metadata, by=c("Tumor.ID"="sample")) %>% 
+  dplyr::filter(cohort == "Derivation") %>% 
+  dplyr::mutate(VarID = stringr::str_c(Normal.ID, Chr, Start, REF, ALT, Gene))
+
+df3 <- df %>% 
+  dplyr::filter(IID1 %in% df2$Tumor.ID) %>% 
+  dplyr::select(IID1, IID2)
+
+shared_vars <- vector(mode = "numeric", length = length(df3))
+total_vars <- vector(mode = "numeric", length = length(df3))
+tumor_1_vars <- vector(mode = "numeric", length = length(df3))
+tumor_2_vars <- vector(mode = "numeric", length = length(df3))
+
+for (row in 1:nrow(df3)) {
+  tmp1 <- df2 %>% dplyr::filter(Tumor.ID == df3[row, "IID1"]$IID1)
+  tmp2 <- df2 %>% dplyr::filter(Tumor.ID == df3[row, "IID2"]$IID2)
+  tumor_1_vars[row] <- nrow(tmp1)
+  tumor_2_vars[row] <- nrow(tmp2)
+  shared_vars[row] <- length(intersect(tmp1$VarID, tmp2$VarID))
+  total_vars[row] <- length(tmp1$VarID) + length(tmp2$VarID) - shared_vars
+}
+
+df3$shared_vars <- shared_vars
+df3$total_vars <- total_vars
+df3$tumor_1_vars <- tumor_1_vars
+df3$percent <- df3$shared_vars / df3$tumor_1_vars * 100
+df3$cat <- "cat"
+
+mean(df3$percent)
+
+plot <- ggplot(df3, aes(x = cat, y=percent)) + 
+  geom_beeswarm(size = 3, cex = 3, color = "#7260a6") + xlab("") + 
+  geom_beeswarm(data = df, aes(x = cat, y = PI_HAT * 100), cex = 2, size = 3, color = "#6a9e3b") + 
+  theme_minimal() + theme(strip.background = element_blank(),
+                               strip.text = element_blank(),
+                               panel.border = element_blank(),
+                               #panel.background = element_blank(),
+                               plot.title = element_text(size = 16, hjust = 0.5),
+                               axis.text.x = element_blank(),
+                               axis.text.y = element_blank(),
+                               axis.title.x = element_blank(),
+                               axis.title.y = element_blank(),
+                               legend.text = element_text(size = 16),
+                               text = element_text(size = 16),
+                               panel.grid.major.x = element_blank(),
+                               panel.grid.minor.x = element_blank(), 
+                               legend.position = "none",
+                               plot.margin = unit(c(0, 0, 0, 0), "cm"))
+
+pdf("results/figures/clonality_analysis/relatedness.pdf", width = 5, height = 5)
+print(plot)
+dev.off()
