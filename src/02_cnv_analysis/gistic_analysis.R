59adb13847af3bdd90f424750349df755bf728a3
Andrew Pregnall <ampregnall@gmail.com>
2025-01-26 11:21:07 -0500

    Update gistic_analysis.R
    * Prepares data for GISTIC analysis using Sequenza output
    * Visualizes GISTIC results using maftools coGisticChromPlot
    * Identifies CGC genes impacted in GISTIC peaks

--- src/02_cnv_analysis/gistic_analysis.R
+++ src/02_cnv_analysis/gistic_analysis.R
@@ -1,7 +1,27 @@
+## ---------------------------
+## Script Name: 01_gistic_analysis.R
+## Description: Create GISTIC and IGV input. Plot results of GISTIC analysis 
+##
+## Author: Andrew M. Pregnall
+## Email: andrew.pregnall@pennmedicine.upenn.edu
+## 
+## Date Created: 2025-01-24
+## Copyright (c) Andrew Pregnall, 2025
+## ---------------------------
+
+# Load packages
 library(purrr)
 library(stringr)
 library(dplyr)
 library(readr)
+library(maftools)
+
+# Define variables
+meta <- readxl::read_xlsx("metadata/mPPGL-Metadata.xlsx", sheet = "tumors")
+prim <- filter(meta, category == "Primary")$sample
+met <- filter(meta, category == "Metastatic")$sample
+
+# Load data -----------------------------------------------------------
 
 # Load all files in directory
 files <- list.files(
@@ -17,16 +37,14 @@ data_list <- map(files, function(file) {
 # Combine all data frames
 df <- bind_rows(data_list)
 
-# Prepare data for GISTIC analysis
-df <- df %>% dplyr::select(sample, chromosome, start.pos, end.pos, N.BAF, depth.ratio) %>%
-  dplyr::distinct(sample, chromosome, start.pos, end.pos, N.BAF, depth.ratio) %>% 
-  dplyr::mutate(Seg.CN = log2(2 * depth.ratio) - 1) %>%
-  dplyr::select(sample, chromosome, start.pos, end.pos, N.BAF, Seg.CN)
+# Preprocess data for GISTIC ----------------------------------------------
 
-# Load metadata
-meta <- readxl::read_xlsx("metadata/mPPGL-Metadata.xlsx", sheet = "tumors")
-prim <- dplyr::filter(meta, category == "Primary")$sample
-met <- dplyr::filter(meta, category == "Metastatic")$sample
+# Prepare data for GISTIC analysis
+df <- df %>% 
+  select(sample, chromosome, start.pos, end.pos, N.BAF, depth.ratio) %>%
+  distinct(sample, chromosome, start.pos, end.pos, N.BAF, depth.ratio) %>% 
+  mutate(Seg.CN = log2(2 * depth.ratio) - 1) %>%
+  select(sample, chromosome, start.pos, end.pos, N.BAF, Seg.CN)
 
 # Subset data
 df.prim <- df %>% filter(sample %in% prim)
@@ -34,21 +52,22 @@ df.met <- df %>% filter(sample %in% met)
 
 # Save data
 write_delim(df, "data/processed/cnvs/gistic/mPPGL.gistic.input.all.txt", delim = "\t")
+write_delim(df, "data/processed/cnvs/gistic/mPPGL.gistic.input.all.seg", delim = "\t")
 write_delim(df.prim, "data/processed/cnvs/gistic/mPPGL.gistic.input.primaries.txt", delim = "\t")
 write_delim(df.met, "data/processed/cnvs/gistic/mPPGL.gistic.input.metastatic.txt", delim = "\t")
 
+
+# RUN GISTIC ------------------------------------------------------------------
+
+## ---------------------------
+## Output files above were used to run GISTIC using the GenePattern server
+## Results were manually saved to output directors below
+## Seg file above was loaded into IGV to create visualization of CNVs
+## ---------------------------
+
 # GISTIC plotting -------------------------------------------------------
 
-library(maftools)
-source("src/utilities/gistic_coplot.R")
-source("src/utilities/readSegs.R")
-
 # Load GISTIC results
-gistic.all = readGistic(gisticAllLesionsFile = "data/processed/cnvs/gistic/gistic_all/all_lesions.conf_90.txt", 
-                        gisticAmpGenesFile = "data/processed/cnvs/gistic/gistic_all/amp_genes.conf_90.txt",
-                        gisticDelGenesFile = "data/processed/cnvs/gistic/gistic_all/del_genes.conf_90.txt",
-                        gisticScoresFile = "data/processed/cnvs/gistic/gistic_all/scores.gistic")
-
 gistic.met = readGistic(gisticAllLesionsFile = "data/processed/cnvs/gistic/gistic_metastatic/all_lesions.conf_90.txt", 
                          gisticAmpGenesFile = "data/processed/cnvs/gistic/gistic_metastatic/amp_genes.conf_90.txt",
                          gisticDelGenesFile = "data/processed/cnvs/gistic/gistic_metastatic/del_genes.conf_90.txt",
@@ -59,24 +78,25 @@ gistic.prim = readGistic(gisticAllLesionsFile = "data/processed/cnvs/gistic/gist
                          gisticDelGenesFile = "data/processed/cnvs/gistic/gistic_primary/del_genes.conf_90.txt",
                          gisticScoresFile = "data/processed/cnvs/gistic/gistic_primary/scores.gistic")
 
-# Create overall plot
-gisticChromPlot(gistic = gistic.all, markBands = "all")
-
-### Save results
+# Save results
 pdf("results/figures/cnv_analysis/gistic_coplot_amp.pdf", width = 9, height = 12)
-print(coGisticChromPlot(gistic1 = gistic.prim, 
-                        gistic2 = gistic.met, 
-                        g1Name = "Primary", 
-                        g2Name = "Metastatic", 
-                        type = 'Amp'))
-dev.off()
-
-
-
-pdf("results/figures/cnv_analysis/gistic_coplot_del.pdf", width = 9, height = 12)
-print(coGisticChromPlot(gistic1 = gistic.prim, 
+coGisticChromPlot(gistic1 = gistic.prim, 
                   gistic2 = gistic.met, 
                   g1Name = "Primary", 
-                  g2Name = "Metastatic", type = 'Del'))
+                  g2Name = "Metastatic", 
+                  type = 'Amp')
 dev.off()
 
+pdf("results/figures/cnv_analysis/gistic_coplot_del.pdf", width = 9, height = 12)
+coGisticChromPlot(gistic1 = gistic.prim, 
+                  gistic2 = gistic.met, 
+                  g1Name = "Primary", 
+                  g2Name = "Metastatic", 
+                  type = 'Del', )
+dev.off()
+
+# Load CGC data and filter GISTIC results for annotations
+cgc <- read_delim("metadata/cancer_census_genes_all_v98.tsv", delim = "\t")
+cgc <- filter(cgc, Tier == 1)
+met.genes <- gistic.met@gene.summary %>% filter(Hugo_Symbol %in% cgc$'Gene Symbol')
+prim.genes <- gistic.prim@gene.summary %>% filter(Hugo_Symbol %in% cgc$'Gene Symbol')
